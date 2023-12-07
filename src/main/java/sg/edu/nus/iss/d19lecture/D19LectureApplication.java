b078bdf23990c9c76de2412f5b9ab91e6f433275
JD-KE <60773497+JD-KE@users.noreply.github.com>
2023-12-07 09:18:44 +0800

    First Commit

---
+++ src/main/java/sg/edu/nus/iss/d19lecture/D19LectureApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.d19lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D19LectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D19LectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/d19lecture/D19LectureApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.d19lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D19LectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
